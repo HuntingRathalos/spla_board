47b2911a8cadec2ca4095fa6989a2961e0983863
HuntingRathalos <92079024+HuntingRathalos@users.noreply.github.com>
2023-08-05 04:35:22 +0900

    Update api.php

--- api/routes/api.php
+++ api/routes/api.php
@@ -30,6 +30,12 @@ Route::middleware('auth:sanctum')->get('/test', function (Request $request) {
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 // Route::get('/test', function (Request $request) {
 //     return response()->json("OK", 200);
 // });
